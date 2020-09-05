ab8a2cd17df7565b29018ac9ff7bcfe3a43330ce
bojrick <bojrick@gmail.com>
2020-09-05 05:26:00 -0500

    tiny update

--- data/data_preprocessing.py
+++ data/data_preprocessing.py
@@ -55,14 +55,13 @@ def final_func():
 
     masks_mapping = dict(masks[['COUNTYFP','NEVER']].values)
     risk_mapping = dict(df[['fips','max_people_at_risk']].values)
-    lat_lon['NEVER MASK']=lat_lon['FIPS '].map(masks_mapping)
-    lat_lon['max_risk']=lat_lon['FIPS '].map(risk_mapping)
-
-    lat_lon.dropna(inplace=True)
+    lat_lon['NEVER MASK']=lat_lon['FIPS '].map(masks_mapping,na_action='ignore')
+    lat_lon['max_risk']=lat_lon['FIPS '].map(risk_mapping,na_action='ignore')
+    lat_lon.fillna(0)
     lat_lon['NEVER MASK'] = round(lat_lon['NEVER MASK']*10000)
     lat_lon['max_risk'] = round(lat_lon['max_risk'])
-    lat_lon['NEVER MASK'] = lat_lon['NEVER MASK'].astype(int)
-    lat_lon['max_risk'] = lat_lon['max_risk'].astype(int)
+    #lat_lon['NEVER MASK'] = lat_lon['NEVER MASK'].astype(int)
+    #lat_lon['max_risk'] = lat_lon['max_risk'].astype(int)
     lat_lon['Hover'] = lat_lon['County ']+'<br>'+'Every 10,000 People '+lat_lon['NEVER MASK'].astype(str)+' Not using Mask'+'<br>'+'Max of '+lat_lon['max_risk'].astype(str)+' People can get Infected'
     lat_lon['FIPS '] = lat_lon['FIPS '].apply(lambda x: str(x).zfill(5))
     print('As CSV saved')
